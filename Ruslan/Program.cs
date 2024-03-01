d4d20f26570d7a5c06bce3b0d5a7ec01ce36152e
Ruslan <saparbaev.ruslan53@gmail.com>
2024-03-01 13:44:17 +0300

    Выполнен ввод значения x

--- Ruslan/Program.cs
+++ Ruslan/Program.cs
@@ -10,7 +10,9 @@ namespace Ruslan
     {
         static void Main(string[] args)
         {
-
+            Console.Write(" Введите значения параметра x = ");
+            string s = Console.ReadLine();
+            double x = Convert.ToDouble(s);
 
 
 
